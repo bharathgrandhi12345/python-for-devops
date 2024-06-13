7bfa2f3d7cbc4967770857ac9034ac046622ca1a
bharathgrandhi12345 <114501122+bharathgrandhi12345@users.noreply.github.com>
2024-06-13 13:27:03 +0530

    Update list_projects.py

--- Day-14/examples/list_projects.py
+++ Day-14/examples/list_projects.py
@@ -4,11 +4,11 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://veeramallaabhishek.atlassian.net/rest/api/3/project"
+url = "https://bharathgrandhi45.atlassian.net/rest/api/3/project"
 
-API_TOKEN=""
+API_TOKEN="ATATT3xFfGF0Rm2BLKp-eOX16S0uSzMWRfe9Pf2ahhLeBt4oTWZV9iIxlMPSMMcf8zUkgEs1hnWIK7a0USYy9armrMo5mjUybf0FtBv3cGGoQ8BzxYYtZKodAcHHnQ-96k9qixaepZ1Cw6J84O6XH92Q8TPJVL8FXOsHt5zlN2fLRg77IGL7QDA=1B5B3162"
 
-auth = HTTPBasicAuth("", API_TOKEN)
+auth = HTTPBasicAuth("bharathgrandhi89@gmail.com",API_TOKEN)
 
 headers = {
   "Accept": "application/json"
